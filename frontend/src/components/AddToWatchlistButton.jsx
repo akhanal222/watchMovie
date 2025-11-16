d6acb8456e0f67fc11dc46b7b1d135d5eefb522a
akhanal222 <ashirvad.khanal@gmail.com>
2025-11-16 18:41:45 -0500

    Added function so after movie is added the button will be disable and display added

--- frontend/src/components/AddToWatchlistButton.jsx
+++ frontend/src/components/AddToWatchlistButton.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 function AddToWatchlistButton({ movie }) {
+   const [disable,setdisable] = useState(false); // this handel the button press 
   async function handleAdd() {
+    setdisable(true);
     try {
       const res = await fetch("http://localhost:3000/api/watchlist/add", {
         method: "POST",
@@ -20,12 +23,13 @@ function AddToWatchlistButton({ movie }) {
       alert("Added to Watchlist!");
     } catch (err) {
       console.error("Add Watchlist Error:", err);
+      setdisable(false); // if there is error let user again press the add button 
     }
   }
 
   return (
-    <button className="add-btn" onClick={handleAdd}>
-      Add to Watchlist
+    <button className="add-btn" onClick={handleAdd} disabled={disable}>
+     {disable? "Added":"Add to Watchlist"} 
     </button>
   );
 }
